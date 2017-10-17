1eaf04a62af019f8f5d13a0967ef65e5b34607e3
Tom Reilly <johndoe@example.com>
2017-10-17 17:00:44 +0100

    added a new view

---
+++ app/src/main/java/com/treilly/portfolio_app/DisplayMessageActivity.java
@@ -0,0 +1,23 @@
+package com.treilly.portfolio_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Extract string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Insert into view
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
--- app/src/main/java/com/treilly/portfolio_app/MainActivity.java
+++ app/src/main/java/com/treilly/portfolio_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.treilly.portfolio_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +50,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static final String EXTRA_MESSAGE = "com.examplemyfirstapp.MESSAGE";
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.text_project);
+        String message =editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
